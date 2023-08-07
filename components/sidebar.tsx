d676e9b620bcdec3a5df6fb2933b01fc1a782c16
David-Koshkadze <rootUS3rr@pm.me>
2023-08-07 11:51:51 +0400

    add side bar icons and sheet for mobile

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,43 +1,60 @@
-"use client"
+"use client";
 
-import { Home, Plus, Settings } from "lucide-react"
-import Link from "next/link"
+import { cn } from "@/lib/utils";
+import { Home, Plus, Settings } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Sidebar() {
+  const pathname = usePathname();
+  const router = useRouter()  
+
   const routes = [
     {
       icon: Home,
       href: "/",
       label: "Home",
-      protected: false,
+      protect: false,
     },
     {
       icon: Plus,
       href: "/companion/new",
       label: "Create",
-      protected: true,
+      protect: true,
     },
     {
       icon: Settings,
       href: "/settings",
       label: "Settings",
-      protected: false,
+      protect: false,
     },
+  ];
 
-  ]
 
+  function onNavigate(url: string, protect: boolean) {
+    return router.push(url)
+  }
 
   return (
     <div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
-      <div className="p-3 flex-1 justify-center">
+      <div className="p-3 flex flex-1 justify-center">
         <div className="space-y-2">
           {routes.map((route, idx) => (
-            <Link href={route.href} key={idx}>
-              <p>{route.label}</p>
-            </Link>
+            <div
+              key={idx}
+              className={cn(
+                "text-muted-foreground text-sm group flex p-3 w-full justify-start cursor-pointer font-medium hover:text-primary hover:bg-primary/10 rounded-lg transition",
+                pathname === route.href ? "bg-primary/10 text-primary" : ""
+              )}
+              onClick={() => onNavigate(route.href, route.protected)}
+            >
+              <div className="flex flex-col gap-y-3 items-center flex-1">
+                <route.icon className="w-5 h-5"/>
+                {route.label}
+              </div>
+            </div>
           ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
