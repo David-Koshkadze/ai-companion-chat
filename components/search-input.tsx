a7b5e52d214a2206a62405f3934a87dd72bda49c
David-Koshkadze <rootUS3rr@pm.me>
2023-08-07 13:11:53 +0400

    implement searching

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,12 +1,57 @@
-import React from 'react'
-import { Input } from './ui/input'
-import { Search } from 'lucide-react'
+"use client";
+
+import React, { ChangeEventHandler, useEffect, useState } from "react";
+import { Input } from "./ui/input";
+import { Search } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useDebounce } from "@/hooks/useDebounce";
+
+import qs from "query-string";
 
 export default function SearchInput() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const categoryId = searchParams.get("categoryId");
+  const name = searchParams.get("name");
+
+  const [value, setValue] = useState(name || "");
+
+  const debouncedValue = useDebounce<string>(value, 700);
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const query = {
+      name: debouncedValue,
+      categoryId: categoryId,
+    };
+
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      {
+        skipEmptyString: true,
+        skipNull: true,
+      }
+    );
+
+    router.push(url);
+  }, [debouncedValue, router, categoryId]);
+
   return (
     <div className="relative">
-      <Search className="absolute w-4 h-4 left-4 top-3"/>
-      <Input placeholder='Search...' className="pl-10 bg-primary/10 text-sm"/>
+      <Search className="absolute w-4 h-4 left-4 top-3" />
+      <Input
+        placeholder="Search..."
+        className="pl-10 bg-primary/10 text-sm"
+        value={value}
+        onChange={onChange}
+      />
     </div>
-  )
+  );
 }
